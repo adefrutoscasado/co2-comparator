4612d83e5bc0abf9036e2d23073ab8cd2dca33a7
Alejandro de Frutos <adefrutos-e@sice-outsourcing.com>
2021-06-08 20:06:09 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,173 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Collapse } from "antd";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  LabelList,
+} from "recharts";
+import { transport, food, streaming, fashion, purchase } from "./data";
+import SliderNumeric from "./components/SliderNumeric";
+import PanelHeader from "./components/PanelHeader";
+import { getTypeProps } from './config'
+import "./App.css";
+const { Panel } = Collapse;
 
-function App() {
+type Key = string;
+
+type BarChartDataItem = {
+  key: Key;
+  checked?: boolean;
+  [key: string]: any;
+};
+type BarChartData = BarChartDataItem[];
+
+const App = () => {
+  const [chartsData, setChartsData] = useState([] as BarChartData);
+  const [fixedAmount, setFixedAmount] = useState(0 as number);
+
+  const insert = (prevState: BarChartData, data: object) => [
+    ...prevState,
+    data,
+  ];
+
+  const update = (
+    prevState: BarChartData,
+    { key, ...data }: BarChartDataItem
+  ): BarChartData => {
+    // @ts-ignore
+    return prevState.map((item) =>
+      item.key === key ? { ...item, ...data } : item
+    );
+  };
+
+  const alreadyExists = (prevState: BarChartData, key: Key): boolean =>
+    !!prevState?.find((item) => item.key === key);
+
+  const onChange = ({ key, ...values }: BarChartDataItem) => {
+    setChartsData((prevState: BarChartData): any[] => {
+      return alreadyExists(prevState, key)
+        ? update(prevState, { key, ...values })
+        : insert(prevState, { key, ...values });
+    });
+  };
+
+  const checkedChartsData = chartsData.filter((chardData) => chardData.checked);
+
+  const generalProps = {
+    onChangeCheck: onChange,
+    onChangeAmount: onChange,
+    fixedAmount: fixedAmount,
+    onSetFixedAmount: setFixedAmount,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <div className="chart-division">
+        <BarChart
+          width={Math.min(1200, window.screen.width)}
+          height={Math.max(400, checkedChartsData.length * 40)}
+          data={checkedChartsData}
+          layout="vertical"
+          margin={{
+            top: 20,
+            right: 100,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="number" />
+          <YAxis type="category" dataKey="key" />
+          <Tooltip />
+          <Bar dataKey="value" stackId="a" fill="#1890ff">
+            <LabelList dataKey="label" position="right" />
+          </Bar>
+        </BarChart>
+      </div>
+      <div className="form-division">
+        <div className="form-container">
+          <Collapse defaultActiveKey={["1"]}>
+            <Panel
+              header={<PanelHeader title="Transport" />}
+              key="1"
+              forceRender
+            >
+              {Object.entries(transport).map(([key, value]) => {
+                return (
+                  <SliderNumeric
+                    {...getTypeProps(key)}
+                    {...generalProps}
+                    name={key}
+                    amountPerStep={value * 1000}
+                  />
+                );
+              })}
+            </Panel>
+            <Panel header={<PanelHeader title="Food" />} key="2" forceRender>
+              {Object.entries(food).map(([key, value]) => {
+                return (
+                  <SliderNumeric
+                    {...getTypeProps(key)}
+                    {...generalProps}
+                    name={key}
+                    amountPerStep={value / 1000}
+                  />
+                );
+              })}
+            </Panel>
+            <Panel
+              header={<PanelHeader title="Streaming" />}
+              key="3"
+              forceRender
+            >
+              {Object.entries(streaming).map(([key, value]) => {
+                return (
+                  <SliderNumeric
+                    {...getTypeProps(key)}
+                    {...generalProps}
+                    name={key}
+                    amountPerStep={value * 60 * 60}
+                  />
+                );
+              })}
+            </Panel>
+            <Panel header={<PanelHeader title="Fashion" />} key="4" forceRender>
+              {Object.entries(fashion).map(([key, value]) => {
+                return (
+                  <SliderNumeric
+                    {...getTypeProps(key)}
+                    {...generalProps}
+                    name={key}
+                    amountPerStep={value}
+                  />
+                );
+              })}
+            </Panel>
+            <Panel
+              header={<PanelHeader title="Purchase" />}
+              key="5"
+              forceRender
+            >
+              {Object.entries(purchase).map(([key, value]) => {
+                return (
+                  <SliderNumeric
+                    {...getTypeProps(key)}
+                    {...generalProps}
+                    name={key}
+                    amountPerStep={value}
+                  />
+                );
+              })}
+            </Panel>
+          </Collapse>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SliderNumeric.tsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { Slider, InputNumber, Checkbox } from 'antd'
+import { EyeTwoTone } from '@ant-design/icons';
+import 'antd/dist/antd.css'
+
+export const formatDecimal = (num = 0, decimals = 2, removeZeroDecimals = true) => {
+    const fixed = Number(num).toFixed(decimals)
+    // @ts-ignore
+    if ((fixed - Math.floor(fixed) === 0) && removeZeroDecimals) return Math.floor(fixed)
+    else return fixed
+}
+
+
+const IntegerStep = ({
+    name = 'name',
+    defaultValue = 1,
+    defaultChecked = false,
+    min = 1,
+    max = 500,
+    onChange = (arg: {key: string, value: number}) => {},
+    onChangeCheck = (arg: {key: string, checked: boolean}) => {},
+    onChangeAmount = (arg: {key: string, value: number, label: string}) => {},
+    onSetFixedAmount = (fixedAmount: any) => {},
+    amountPerStep = 1,
+    amountUnits = 'kgCO2eq',
+    stepUnit = '',
+    fixedAmount = 0
+}) => {
+    const [ inputValue, setInputValue ] = useState(defaultValue)
+    const [ checked, setChecked ] = useState(defaultChecked)
+    const [ focused, setFocused ] = useState(false)
+
+    useEffect(() => {
+        if (fixedAmount) {
+            const inputValue = fixedAmount / amountPerStep
+            setInputValue(inputValue)
+        }
+    }, [fixedAmount])
+
+    useEffect(() => {
+        onChangeCheck({key: name, checked: checked})
+    }, [checked])
+
+    const onChangeValue = (value: number) => {
+        setInputValue(value);
+    }
+
+    useEffect(() => {
+        onChange({key: name, value: inputValue})
+    }, [inputValue])
+
+    const amount = (typeof amountPerStep === 'function') ?
+        // @ts-ignore
+        amountPerStep(inputValue)
+        :
+        amountPerStep * inputValue
+
+    const label = `${formatDecimal(inputValue)} ${stepUnit} of ${name}`
+
+    useEffect(() => {
+        onChangeAmount({key: name, value: amount, label})
+    }, [])
+
+    useEffect(() => {
+        onChange({key: name, value: inputValue})
+        onChangeAmount({key: name, value: amount, label})
+    }, [inputValue])
+    
+    return (
+        <div className='integer-step'>
+            <div className='flex-direction-row'>
+                <div className='checkbox'>
+                    <Checkbox
+                        onChange={() => {
+                            setChecked(checked => !checked);
+                        }}
+                        checked={checked}
+                    >
+                    </Checkbox>
+                </div>
+                <div className='label'>
+                    <div className='flex-direction-row space-between'>
+                        <div>
+                            <h4 onClick={() => {
+                                onSetFixedAmount(amount)
+                            }}>
+                                {name}
+                            </h4>
+                        </div>
+                        <div className='flex-direction-row space-between'>
+                            <div>
+                                <h4>
+                                    {formatDecimal(amount)} {amountUnits}
+                                </h4>
+                            </div>
+                            <div 
+                                className='check-equivalences-icon'
+                                title="Check equivalences"
+                            >
+                                <EyeTwoTone 
+                                    onClick={() => {
+                                        onSetFixedAmount(amount)
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='flex-direction-row space-between'>
+                <div className='slider'>
+                    <Slider
+                        min={min}
+                        max={max}
+                        onChange={onChangeValue}
+                        onAfterChange={() => onChangeAmount({key: name, value: amount, label})}
+                        value={typeof inputValue === "number" ? inputValue : 0}
+                    />
+                </div>
+                <div className='input-number'>
+                    <InputNumber
+                        min={min}
+                        max={max}
+                        value={inputValue}
+                        onChange={onChangeValue}
+                        onFocus={() => {
+                            setFocused(true)
+                            // REVIEW: suming triggers a change to format again 
+                            setInputValue(inputValue+0.00000000000001)
+                        }}
+                        onBlur={() => {
+                            setFocused(false)
+                            // REVIEW: suming triggers a change to format again 
+                            setInputValue(inputValue+0.00000000000001)
+                        }}
+                        formatter={
+                            focused ? 
+                                value => `${formatDecimal(value)}` 
+                                : 
+                                value => `${formatDecimal(value)} ${stepUnit}`
+                            }
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default IntegerStep
